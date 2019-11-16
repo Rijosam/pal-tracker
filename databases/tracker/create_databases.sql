ecd9a903ef1571729366b7e35c92bb123910b661
rijosam <rijosam19@gmail.com>
2019-11-16 10:26:07 +0530

    Travis build fix

--- databases/tracker/create_databases.sql
+++ databases/tracker/create_databases.sql
@@ -4,6 +4,6 @@ DROP DATABASE IF EXISTS tracker_test;
 CREATE DATABASE tracker_dev;
 CREATE DATABASE tracker_test;
 
-CREATE USER 'tracker' @'localhost' IDENTIFIED BY '';
+CREATE USER IF NOT EXISTS 'tracker' @'localhost' IDENTIFIED BY '';
 GRANT ALL PRIVILEGES ON tracker_dev.* TO 'tracker' @'localhost';
 GRANT ALL PRIVILEGES ON tracker_test.* TO 'tracker' @'localhost';
--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -4,12 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
 public class PalTrackerApplication {
 
+    private DataSource dataSource;
+
+    public PalTrackerApplication(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     TimeEntryRepository timeEntryRepository() {
-        return new InMemoryTimeEntryRepository();
+        return new JdbcTimeEntryRepository(dataSource);
     }
 
     public static void main(String[] args) {
